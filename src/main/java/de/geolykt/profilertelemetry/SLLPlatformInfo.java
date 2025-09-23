ea3e5a9a21a930ce8a0dca8b1731688d53b9f765
Geolykt <admin@geolykt.de>
2025-09-24 00:38:15 +0200

    Use new SLLEnvironment API to obtain platform information

--- src/main/java/de/geolykt/profilertelemetry/SLLPlatformInfo.java
+++ src/main/java/de/geolykt/profilertelemetry/SLLPlatformInfo.java
@@ -1,58 +1,25 @@
 package de.geolykt.profilertelemetry;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Properties;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.LoggerFactory;
+import org.stianloader.sll.SLLEnvironment;
 
-import de.geolykt.starloader.Starloader;
 import de.geolykt.starloader.api.resource.DataFolderProvider;
 
 import me.lucko.spark.common.platform.PlatformInfo;
 
 public class SLLPlatformInfo implements PlatformInfo {
 
-    @Nullable
-    private static Map.Entry<@NotNull String, @NotNull String> determineLauncherMeta() {
-        for (String variant : new @NotNull String[] {
-                "launcher-micromixin",
-                "launcher-sponge",
-                "launcher-mixin-fabric"
-        }) {
-            try (InputStream is = Starloader.class.getClassLoader().getResourceAsStream("META-INF/maven/org.stianloader/" + variant + "/pom.properties")) {
-                if (is == null) {
-                    continue;
-                }
-                Properties properties = new Properties();
-                properties.load(is);
-                return new AbstractMap.SimpleImmutableEntry<>(variant, Objects.requireNonNull(properties.getProperty("version"), "property file should contain version field"));
-            } catch (IOException ignored) { }
-        }
-
-        LoggerFactory.getLogger(SLLPlatformInfo.class).warn("Unable to determine SLL brand information!");
-        return null;
-    }
-
     @NotNull
     private final String applicationName;
 
     @NotNull
     private final String applicationVersion;
-
-    @NotNull
-    private final String sllVariant;
-
-    @NotNull
-    private final String sllVersion;
 
     public SLLPlatformInfo() {
         String galimVersion = "unknown";
@@ -80,33 +47,16 @@ public class SLLPlatformInfo implements PlatformInfo {
 
         this.applicationName = applicationName;
         this.applicationVersion = galimVersion;
-
-        Map.Entry<@NotNull String, @NotNull String> sllMeta = SLLPlatformInfo.determineLauncherMeta();
-        if (sllMeta == null) {
-            this.sllVariant = "unknown";
-            this.sllVersion = "unknown";
-        } else {
-            this.sllVariant = sllMeta.getKey();
-            this.sllVersion = sllMeta.getValue();
-        }
     }
 
     public SLLPlatformInfo(@NotNull String applicationVersion, @NotNull String applicationName) {
         this.applicationVersion = applicationVersion;
         this.applicationName = applicationName;
-        Map.Entry<@NotNull String, @NotNull String> sllMeta = SLLPlatformInfo.determineLauncherMeta();
-        if (sllMeta == null) {
-            this.sllVariant = "unknown";
-            this.sllVersion = "unknown";
-        } else {
-            this.sllVariant = sllMeta.getKey();
-            this.sllVersion = sllMeta.getValue();
-        }
     }
 
     @Override
     public String getBrand() {
-        return "SLL (" + this.sllVariant + ")";
+        return "SLL (" + SLLEnvironment.getSLLGroupId() + ":" + SLLEnvironment.getSLLArtifactId() + " using " + SLLEnvironment.getMixinCompiledGroupId() + ":" + SLLEnvironment.getMixinCompiledArtifactId() + ")";
     }
 
     @Override
@@ -126,6 +76,6 @@ public class SLLPlatformInfo implements PlatformInfo {
 
     @Override
     public String getVersion() {
-        return this.sllVersion;
+        return SLLEnvironment.getSLLVersion() + " with mixin version " + SLLEnvironment.getMixinCompiledVersion();
     }
 }
